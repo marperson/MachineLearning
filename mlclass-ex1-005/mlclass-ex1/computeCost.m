b5416466340631e9f8644e30b160b1c91df53a1c
Frank <marperson@hotmail.com>
2014-03-25 00:49:00 -0400

    Add ComputeCost and GradientDescent

--- mlclass-ex1-005/mlclass-ex1/computeCost.m
+++ mlclass-ex1-005/mlclass-ex1/computeCost.m
@@ -13,10 +13,21 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-J = COMPUTECOST(X, y, theta);
+% ================================================================================
+% No vectorized code
+%tmp=0;
+%for i=1:m, 
+%    tmp = tmp+ (theta (1,1) + theta (2,1)*X(i,2) - y(i))^2; %differenzberechnung
+%end
 
+%J= (1/(2*m)) * tmp;
+
+% =========================================================================
+% Vectorized code
+predictions = X * theta;
+sqrerror = (predictions - y).^2;
+J = (1/(2*m))*sum(sqrerror);
 
 
 % =========================================================================
-
 end
